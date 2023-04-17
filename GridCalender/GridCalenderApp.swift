b195ed8bb64af2e4234186542fea5beae1762a81
Esther Tsui <66723850+esther-tsui@users.noreply.github.com>
2023-04-17 15:38:39 +0100

    first commit

---
+++ GridCalender/GridCalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalenderApp.swift
+//  GridCalender
+//
+//  Created by Wing Hei Tsui on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct GridCalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
